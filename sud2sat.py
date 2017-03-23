125fbbc1350d35975592b7874a20c1e85eeac131
mabsaleh <abousaleh.mohammed@gmail.com>
2017-03-23 16:35:34 -0700

    Add function for generating clauses for second rule

--- sud2sat.py
+++ sud2sat.py
@@ -1,7 +1,17 @@
+#There is at least one number in each entry
 def fstRule(s):
 	for x in range(1, 10):
 		for y in range(1, 10):
 			for z in range(1, 10):
 				s += str(x) + str(y) + str(z) + ' '
 			s += '0\n'
+	return s
+
+#Each number appears at most once in each column
+def thrdRule(s):
+	for x in range(1, 10):
+		for z in range(1, 10):
+			for y in range(1, 9):
+				for i in range(y+1, 10):
+					s += '-' + str(x) + str(y) + str(z) + ' ' + '-' + str(x) + str(i) + str(z) + ' 0\n'
 	return s

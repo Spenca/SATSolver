2bdaf37ee0b5b432bcbfdcfe180f6ffb508f0b29
Yuki Hayashi <yuki_hayashi@hotmail.com>
2017-03-23 16:45:33 -0700

    added second function

--- sud2sat.py
+++ sud2sat.py
@@ -1,7 +1,28 @@
+#There is at least one number in each entry
 def fstRule(s):
 	for x in range(1, 10):
 		for y in range(1, 10):
 			for z in range(1, 10):
 				s += str(x) + str(y) + str(z) + ' '
 			s += '0\n'
+	return s
+
+#Each number appears at most once in each row:
+def secondRule(s):
+	for y in range(1,9):
+		for z in range(1,9):
+			for x in range(1,8):
+				for i in range(x+1,9):
+					
+					s1 += '-' + str(x) +str(y) + str(z) + ' '  + '-' + str(i) + str(y) + str(z) + ' ' + '0\n'
+			
+	return s
+	
+#Each number appears at most once in each column
+def thrdRule(s):
+	for x in range(1, 10):
+		for z in range(1, 10):
+			for y in range(1, 9):
+				for i in range(y+1, 10):
+					s += '-' + str(x) + str(y) + str(z) + ' ' + '-' + str(x) + str(i) + str(z) + ' 0\n'
 	return s

73df8eea1758fd0f06f1d0eda594c880743bd4d5
mabsaleh <abousaleh.mohammed@gmail.com>
2017-03-23 19:05:14 -0700

    Add SAT to Sudoku converter with test I/O

---
+++ sat2sud.py
@@ -0,0 +1,13 @@
+def genSudoku(s):
+	output = ''
+	for i in s.read().split(" "):
+		try:
+			if int(i) > 0:
+				output += i[2]
+				if (len(output) - output.count('\n')) % 9 == 0:
+					output += '\n'
+		except ValueError:
+			continue
+	print output
+
+genSudoku(open("testSATOutput", "r"))

6213a87a21c9b9ca1674e0fb03d861ae384a78d4
mabsaleh <abousaleh.mohammed@gmail.com>
2017-04-03 20:47:31 -0700

    Fix conversion from SAT to sudoku so it ignores invalid numbers

--- sat2sud.py
+++ sat2sud.py
@@ -2,7 +2,9 @@ def genSudoku(s):
 	output = ''
 	for i in s.read().split(" "):
 		try:
-			if int(i) > 0:
+			if int(i) > 110:
+				if int(i[1]) == 0 or int(i[2]) == 0:
+					continue
 				output += i[2]
 				if (len(output) - output.count('\n')) % 9 == 0:
 					output += '\n'
